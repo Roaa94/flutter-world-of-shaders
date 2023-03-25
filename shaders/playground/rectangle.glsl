963bcd9e04f4023cc3f3865097b6a7cf94b22088
Roaa94 <roaa94khaddam@gmail.com>
2023-03-26 01:11:19 +0300

    chore: experimenting with rectangle shape shader example

--- shaders/playground/rectangle.glsl
+++ shaders/playground/rectangle.glsl
@@ -11,16 +11,8 @@ uniform vec2 uSize;
 
 out vec4 fragColor;
 
-void main() {
-    // The local position of the pixel being evaluated
-    vec2 localPos = FlutterFragCoord().xy;
-
-    // The normalized position of the pixel being evaluated
-    vec2 uv = localPos / uSize;
-
-    // Start with black color
-    vec3 color = vec3(0.0);
-
+// Creates a filled square
+vec3 createSquare(vec2 uv, float distanceFromEdge) {
     // Create the left border with a step function:
     // float left = step(0.1, uv.x);   // Similar to ( X greater than 0.1 )
     //
@@ -32,14 +24,39 @@ void main() {
     //
     // A shorter way of doinf this would be as follows:
     // `left` is now the `x` in `topLeft` and `bottom` is the `y` in `topLeft`
-    vec2 topLeft = step(vec2(0.1), uv);
+    vec2 topLeft = step(vec2(distanceFromEdge), uv);
 
     // We can deduct the bottomRight pixels by inverting the `uv` coordinates
     // This converts the top left corner (0, 0) into the bottom right corner (1, 1)
-    vec2 bottomRight = step(vec2(0.1), 1 - uv);
+    vec2 bottomRight = step(vec2(distanceFromEdge), 1 - uv);
 
     // The outcome color for each pixel is the multiplication of all the square sides pixels
-    color = vec3(topLeft.x * topLeft.y * bottomRight.x * bottomRight.y);
+    return vec3(topLeft.x * topLeft.y * bottomRight.x * bottomRight.y);
+}
+
+// Creates an outline square
+// Todo: optimize
+vec3 createSquareOutline(vec2 uv, float distanceFromEdge, float borderWidth) {
+    vec2 topLeftClipMask = 1.0 - (step(vec2(distanceFromEdge), uv.yx) - step(vec2(1.0 - distanceFromEdge), uv.yx));
+    vec2 topLeft = 1.0 - (step(vec2(distanceFromEdge), uv) - step(vec2(distanceFromEdge + borderWidth), uv));
+    vec2 topLeftFinal = topLeftClipMask + topLeft;
+
+    vec2 bottomRightClipMask = 1.0 - (step(vec2(distanceFromEdge), (1.0 - uv.yx)) - step(vec2(1.0 - distanceFromEdge), (1.0 - uv.yx)));
+    vec2 bottomRight = 1.0 - (step(vec2(distanceFromEdge), (1.0 - uv)) - step(vec2(distanceFromEdge + borderWidth), (1.0 - uv)));
+    vec2 bottomRightFinal = bottomRight + bottomRightClipMask;
+
+    return vec3(topLeftFinal.x * topLeftFinal.y * bottomRightFinal.x * bottomRightFinal.y);
+}
+
+void main() {
+    // The local position of the pixel being evaluated
+    vec2 localPos = FlutterFragCoord().xy;
+
+    // The normalized position of the pixel being evaluated
+    vec2 uv = localPos / uSize;
+
+//    vec3 color = createSquare(uv, 0.1);
+    vec3 color = createSquareOutline(uv, 0.2, 0.02);
 
     // fragColor = vec4(1.0, 0.0, 0.0, 1.0); // Red color
     fragColor = vec4(color, 1.0); // Red color
